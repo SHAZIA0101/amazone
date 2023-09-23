3ad87ecebc923c4da7b6acb40c51f6dbbea4da26
SHAZIA0101 <122517096+SHAZIA0101@users.noreply.github.com>
2023-09-24 01:52:56 +0500

    Update Tweet.tsx my first update

--- src/app/components/Tweet.tsx
+++ src/app/components/Tweet.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Tweet = () => {
   return (
    <div className='texl-lg font-bold ml-10 mt-10'>This is my first Tweet</div>
+   <div className='texl-lg font-bold ml-10 mt-10'>HELLO TWEET</div>
       
 
   )

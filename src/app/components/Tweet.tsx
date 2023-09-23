3ba406ea7f4f92579525254ade60b54c15cee48d
shazia chand <fiverra2z@gmail.com>
2023-09-24 01:49:04 +0500

    change in tweet

--- src/app/components/Tweet.tsx
+++ src/app/components/Tweet.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Tweet = () => {
   return (
-   <div className='texl-lg font-bold ml-10 mt-10'>Tweet</div>
+   <div className='texl-lg font-bold ml-10 mt-10'>This is my first Tweet</div>
       
 
   )

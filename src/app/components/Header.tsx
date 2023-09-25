e26ba4b4aa814ed7b2ca97337cc063ff2b545b6d
shazia chand <fiverra2z@gmail.com>
2023-09-25 15:42:56 +0500

    updated file101

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
-
+import Image from 'next/image'
+import amazonL from "/public/amazon-logo.webp"
+import { text } from 'node:stream/consumers'
+import{TbSearch} from 'react-icons/tb'
+import{FaShoppingCart} from 'react-icons/fa'
+import Link from 'next/link'
 const Header = () => {
   return (
-   <div className='texl-lg font-bold ml-10 mt-10'>This is Header</div>
-      
+    <>
+    <div className='flex w-full px-11 justify-between gap-6 items-center'>
+        <div>
+ <Image src ={amazonL} alt="my logo" className=' w-40  ml-6 '/>
+  </div>
 
+<div className='w-full'>
+ <form className='flex'>
+    <input type="text" className='w-full border rounded-l-xl   py-2 px-2 outline-none
+    focus:border-[#ff9900] font-bold text-gray-100'  placeholder="Search Here"/>
+    <button className='text-2xl bg-[#ff9900]  px-2 rounded-r-xl'><TbSearch />
+    </button>
+   </form>
+
+ </div>
+ <div>
+  <Link href={"/Cart"}></Link>
+  <FaShoppingCart/>
+ </div>
+  </div>
+</>
   )
 }
 

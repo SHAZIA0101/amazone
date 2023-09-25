87cc04f6463c912f6ce45c819f674b8c06f09389
shazia chand <fiverra2z@gmail.com>
2023-09-25 10:01:49 +0500

    my updated file

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+   <div className='texl-lg font-bold ml-10 mt-10'>This is Header</div>
+      
+
+  )
+}
+
+export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
-import Tweet from './components/Tweet'
+import Header from './components/Header'
 
 export default function Home() {
   return (       
-<Tweet/>
+<Header/>
   )
 }
